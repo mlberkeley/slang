aabbbe7578761ba27e81fd0de94400d89372051b
Alvin Zhang <alvn.zng@gmail.com>
2017-04-25 15:19:19 -0700

    get demo and encode_text working

--- src/encode_text.py
+++ src/encode_text.py
@@ -15,35 +15,35 @@ DEFAULT_PARAMS = { 'load':True,
                    'batch_size':1,
                    'kl_alpha_rate':1e-5,
                    'learning_rate':1e-4}
+VOCAB_SIZE = 3700
 
 # encode a single text
 def encode_text(sources, format, seq_len=15, encodeDict=None, decodeDict=None, params=DEFAULT_PARAMS, save_name='', save=False):
     if decodeDict is not None:
         text = data(sources, format, len(decodeDict), seq_len, encodeDict, decodeDict)
     else:
-        text = data(sources, format, 3700, seq_len, encodeDict, decodeDict)
+        text = data(sources, format, VOCAB_SIZE, seq_len, encodeDict, decodeDict)
 
     textLen = len(text.allSentences)
-
-    onehot_text = []
-    for i in range(5): #textLen):
-        sentence = []
-        for word in text.getOneHotSentence(i):
-            sentence.append(np.array(word[0], dtype=np.float32))
-        onehot_text.append(np.array(sentence, dtype=np.float32))
-    onehot_text = np.array(onehot_text, dtype=np.float32)
 
     params['vocab_size'] = len(text.decodeDict)
     params['seq_len'] = seq_len
 
     vsem = VSEM(params)
 
-    book_mu, book_log_var = vsem.sess.run([vsem.mu, vsem.log_var], feed_dict = {vsem.x:onehot_text,
-                                                                                vsem.keep_prob:1.0,
-                                                                                vsem.batch_size:1})
+    book_mu = np.empty((textLen, 256))
+    book_log_var = np.empty((textLen, 256))
+
+    for i in range(len(text.allSentences)): #textLen):
+        sentence = []
+        for word in text.getOneHotSentence(i):
+            sentence.append(word[0])
+        sentence = np.array(sentence, dtype=np.float32)
+
+        book_mu[i], book_log_var[i], _ = vsem.encode(sentence)
 
     if save:
-        np.save(save_name + "_mu.npy", book_mu[0,:])
-        np.save(save_name + "_log_var.npy", book_log_var[0,:])
+        np.save(save_name + "_mu.npy", book_mu)
+        np.save(save_name + "_log_var.npy", book_log_var)
 
-    return book_mu[0,:], book_log_var[0,:]
+    return book_mu, book_log_var
